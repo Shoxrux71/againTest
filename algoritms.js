4d594e9b3e0f9f11f6e7c33083b413a32f47fc82
Shoxrux71 <amirjonovdamin@gmail.com>
2024-04-21 21:37:42 +0500

    no

--- algoritms.js
+++ algoritms.js
@@ -2,20 +2,42 @@ let input1 = document.getElementById('input').value;
 let btn = document.querySelector(".btn");
 let resultNumber = document.querySelector(".result");
 console.log(resultNumber);
+const speedLimits = 70;
+const km_per_pont = 5;
 
-function fizbuz(input) {
-    // if (typeof input !== 'number')
-    //     return 'noooo number';
-    if (input % 3 === 0)
-        return 'Fizz'
-    if (input % 5 === 0)
-        return 'Buzz'
-    if ((input % 3 === 0) && (input % 5 === 0))
-        return 'FizzBuzz';
-
-    return input;
+function checkSpeed(speed) {
+    let exportR = resultNumber;
+    if (speed < speedLimits)
+        console.log('Hammasi Yaxshi');
+    else {
+        let point = Math.floor((speed - speedLimits) / km_per_pont);
+        if (point >= 12) {
+            console.log('eeeeee');
+        } else {
+            exportR = point;
+        }
+    }
 }
 btn.addEventListener('click', function () {
-    let result = fizbuz(input1);
-    resultNumber.innerHTML = result;
+    resultNumber.textContent = checkSpeed(input1);
 })
+
+
+
+
+// function fizbuz(input) {
+//     // if (typeof input !== 'number')
+//     //     return 'noooo number';
+//     if (input % 3 === 0)
+//         return 'Fizz'
+//     if (input % 5 === 0)
+//         return 'Buzz'
+//     if ((input % 3 === 0) && (input % 5 === 0))
+//         return 'FizzBuzz';
+
+//     return input;
+// }
+// btn.addEventListener('click', function () {
+//     let result = fizbuz(input1);
+//     resultNumber.innerHTML = result;
+// })
