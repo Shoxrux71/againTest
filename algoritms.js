386f91ed695f161f27d55c2aee59dfa1d3a03430
Shoxrux71 <amirjonovdamin@gmail.com>
2024-04-21 21:14:24 +0500

    n

--- algoritms.js
+++ algoritms.js
@@ -1,6 +1,11 @@
+let input1 = document.getElementById('input').value;
+let btn = document.querySelector(".btn");
+let resultNumber = document.querySelector(".result");
+console.log(resultNumber);
+
 function fizbuz(input) {
-    if (typeof input !== 'number')
-        return 'noooo number';
+    // if (typeof input !== 'number')
+    //     return 'noooo number';
     if (input % 3 === 0)
         return 'Fizz'
     if (input % 5 === 0)
@@ -10,5 +15,7 @@ function fizbuz(input) {
 
     return input;
 }
-let result = fizbuz('sdsd');
-console.log(result);
+btn.addEventListener('click', function () {
+    let result = fizbuz(input1);
+    resultNumber.innerHTML = result;
+})
